9fd3fa520cb4786b54502173211cbe9214654dfa
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-10-14 18:54:40 +0300

    Решил задание по двумерным массивам

--- DzMassiv/Kinoteatr.cs
+++ DzMassiv/Kinoteatr.cs
@@ -1,28 +1,27 @@
-﻿int[,] arrey = new int[2, 3];
-int[] sumKino = new int[arrey.GetLength(0)];
-int sum = 0;
+﻿int[,] arrey = new int[30, 10];
+Random r = new Random();
 
 for (int i = 0; i < arrey.GetLength(0); i++)
 {
     for (int j = 0; j < arrey.GetLength(1); j++)
     {
-        Console.WriteLine("Y: " + i + " X: " + j);
-        arrey[i, j] = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Y: " + i + " X: " + j);
+        //arrey[i, j] = int.Parse(Console.ReadLine());
+        arrey[i, j] = r.Next(20);
     }
 }
-    Console.WriteLine(sumKino[arrey.GetLength(0)-1]);
+
+int[] sumKino = new int[arrey.GetLength(1)];
 
 for (int i = 0; i < arrey.GetLength(1); i++)
 {
+    int sum = 0;
 
     for (int j = 0; j < arrey.GetLength(0); j++)
     {
-        sum += arrey[i,j];
+        sum += arrey[j,i];
     }
 
-    for (int k = 0; k < arrey.GetLength(0); k++)
-    {
-        sumKino[k] = sum;
-    }
-    Console.WriteLine(sumKino[i]);
+    sumKino[i] = sum;
+    Console.WriteLine($"Сумма простмотров фильма № {i+1} за месяц составляет: {sumKino[i]}");
 }
