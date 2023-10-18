0d11d7a9e9efac4b056b5571b6b391629313d413
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-10-18 16:46:02 +0300

    добавил задание по зоопарку

--- Animals/Lion.cs
+++ Animals/Lion.cs
@@ -8,6 +8,18 @@ namespace Animals
 {
     public class Lion
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string View { get; set; }
+        public string Habitat { get; set; }
+        public double Area { get; set; }
+        public string Food { get; set; }
+        public string Sound { get; set; }
+
+        public void DoVoice()
+        {
+            Console.WriteLine($"{Name} Pppp-Ppp");
+        }
 
     }
 }
